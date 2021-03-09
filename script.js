96908b02ead2a2b4b3a31c976d47162034eae4d6
kpt2105 <61200949+kpt2105@users.noreply.github.com>
2021-03-09 18:44:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var x;
+function calc_0() {
+  x = document.getElementById("browser").value;
+  console.log(x);
+  if (x == "eV to Joule") {
+    document.location = "/main-calc/index_main_calc-0.html";
+  } else if (x == "Speed of light to km/h") {
+    document.location = "/main-calc/index_main_calc-1.html";
+  }
+  return false;
+}
