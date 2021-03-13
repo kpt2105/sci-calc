2014e2ef0d29dd8ec9a30a940ffbb689b70881ef
kpt2105 <61200949+kpt2105@users.noreply.github.com>
2021-03-14 00:51:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function calc_0() {
   x = document.getElementById("browser").value;
   console.log(x);
   if (x == "eV to Joule") {
-    document.location = "/main-calc/index_main_calc-0.html";
+    document.location = "/main-calc/index_main_calc-0";
   } else if (x == "Speed of light to km/h") {
-    document.location = "/main-calc/index_main_calc-1.html";
+    document.location = "/main-calc/index_main_calc-1";
   }
   return false;
 }
