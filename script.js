a02eef8a86e9fa84a7dbcfcc5c11c53a038f4985
kpt2105 <61200949+kpt2105@users.noreply.github.com>
2021-03-21 01:45:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ function calc_0() {
   x = document.getElementById("browser").value;
   console.log(x);
   if (x == "eV to Joule") {
-    document.location = "/main-calc/index_main_calc-0";
+    document.location = "/main-calc/index_main_calc-0.html";
   } else if (x == "Speed of light to km/h") {
-    document.location = "/main-calc/index_main_calc-1";
+    document.location = "/main-calc/index_main_calc-1.html";
   } else if (x == "Seconds to Planck Times") {
     document.location = "/main-calc/calc2.html";
+  } else if (x == "Meters to Planck Lengths") {
+    document.location = "/main-calc/calc3.html";
   }
   return false;
 }
 function contact() {
-   window.location.href = "/contact";
+  window.location.href = "/contact.html";
 }
