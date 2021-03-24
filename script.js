f4d8b670d63318deee59e2690ee8d5d4f9233e42
kpt2105 <61200949+kpt2105@users.noreply.github.com>
2021-03-24 16:23:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function calc_0() {
   x = document.getElementById("browser").value;
   console.log(x);
   if (x == "eV to Joule") {
-    document.location = "/main-calc/index_main_calc-0";
+    document.location = "/calcs/calc0";
   } else if (x == "Speed of light to km/h") {
-    document.location = "/main-calc/index_main_calc-1";
+    document.location = "/calcs/calc1";
   } else if (x == "Seconds to Planck Times") {
-    document.location = "/main-calc/calc2";
+    document.location = "/calcs/calc2";
   } else if (x == "Meters to Planck Lengths") {
-    document.location = "/main-calc/calc3";
+    document.location = "/calcs/calc3";
   } else if (x == "Lightyears to Km") {
-    document.location = "/main-calc/calc4";
+    document.location = "/calcs/calc4";
   }
   return false;
 }
